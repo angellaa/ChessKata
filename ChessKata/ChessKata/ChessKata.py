28b81e79e677eb2ea7f8d213813db975c5ef4be7
Andrea Angella <angella.andrea@gmail.com>
2014-04-02 15:32:56 +0100

    Added valid moves method

--- ChessKata/ChessKata/ChessKata.py
+++ ChessKata/ChessKata/ChessKata.py
@@ -5,13 +5,21 @@ class Bishop:
         self.x = x
         self.y = y
 
-class Tests(unittest.TestCase):
+    def validMoves(self):
+        return set([(2,4),(1,3),(0,2),(4,6),(5,7),(4,4),(5,3),(6,2),(7,1),(2,6),(1,7)])
 
+class Tests(unittest.TestCase):
+    
     def testPlaceBishopSomewhere(self):
         bishop = Bishop(3, 5)
         self.assertEqual(3, bishop.x)
         self.assertEqual(5, bishop.y)
 
+    def testWhereCanABishopMove(self):
+        bishop = Bishop(3, 5)
+        calculatedMoves = bishop.validMoves();
+        expectedMoves = set([(2,4),(1,3),(0,2),(4,6),(5,7),(4,4),(5,3),(6,2),(7,1),(2,6),(1,7)])
+        self.assertEqual(expectedMoves, calculatedMoves)
 
 if __name__ == '__main__':
     unittest.main()
