d83d9baee2737578f37fdb32fa604ac92eecb15d
Andrea Angella <angella.andrea@gmail.com>
2014-04-02 15:20:19 +0100

    Place a bishop anywhere on the board :)

--- ChessKata/ChessKata/ChessKata.py
+++ ChessKata/ChessKata/ChessKata.py
@@ -1,1 +1,18 @@
-print('Hello World')
+import unittest
+
+class Bishop:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Tests(unittest.TestCase):
+
+    def test(self):
+        bishop = Bishop(3, 5)
+        self.assertEqual(3, bishop.x)
+        self.assertEqual(5, bishop.y)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
